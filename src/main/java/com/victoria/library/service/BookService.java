475c8d1663bedad580fc617371f9c1b1a390cab7
Victoria Paiva <victoriardspaiva@recife.pe.senac.br>
2023-06-11 16:37:24 -0300

    swagger com configurações iniciais

--- src/main/java/com/victoria/library/service/BookService.java
+++ src/main/java/com/victoria/library/service/BookService.java
@@ -3,10 +3,12 @@ package com.victoria.library.service;
 import com.victoria.library.entity.Book;
 import com.victoria.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class BookService {
@@ -14,19 +16,24 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public Book save(Book library){
-        return bookRepository.save(library);
+    public Book save(Book book){
+        return bookRepository.save(book);
     }
 
     public List<Book> getAll(){
         return bookRepository.findAll();
     }
 
-    public Optional<Book> getByID(Long id){
+    public Optional<Book> getByID(UUID id){
         return bookRepository.findById(id);
     }
 
-    public void deleteById(Long id){
+    public void deleteById(UUID id){
         bookRepository.deleteById(id);
     }
+
+//    public Book changeStatus(Optional<Book> book, Boolean status) {
+//        book.get().setReadStatus(status);
+//        return bookRepository.save(book);
+//    }
 }
